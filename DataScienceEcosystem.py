9930a0427e50011430e3b64f650833e88ac1eae2
markusohd <131212300+markusohd@users.noreply.github.com>
2023-09-16 18:32:38 +0800

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+# 
+# 
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# **Objectives:**
+# 
+# * Describe R capabilities and RStudio environment.
+# * Use the inbuilt R plot function.
+# * Explain version control and describe the Git and GitHub environment.
+# * Describe the purpose of source repositories and explain how GitHub satisfies the needs of a source repository.
+# * Explain how to sign up for a GitHub account and describe how to create a repository.
+# * Demonstrate how to edit and upload files in GitHub.
+# * Explain the purpose of branches and describe how to merge changes into branches.
+
+# Some of the popular languages that Data Scientists use are:
+# 1. R
+# 2. Python
+# 3. Julia
+
+# Some of the commonly used libraries used by Data Scientists include:
+# 1. ggplot2
+# 2. dyplr
+# 3. matplotlib
+# 4. caret
+# 5. scikit-learn
+
+# | Data Sciecne Tools |
+# ______________________
+# RStudio
+# 
+# Apache Spark
+# 
+# Apache Hadoop
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python
+
+# In[2]:
+
+
+# This a simple arithmetic expression to mutiply then add integers
+(3*4)+5
+
+
+# In[3]:
+
+
+# This will convert 200 minutes to hours by diving by 60
+# 200 minutes into hours
+200/60
+
+
+# ## Author
+# Mark Lawrence Nicdao
